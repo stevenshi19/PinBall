c3875eed8ed81b3e225784282b17a0e7a0a14dd2
stevenshi19 <stevenwaseda87@gmail.com>
2020-07-28 21:15:12 +0900

    Pinballプロジェクトになります 初めてのコミットとなります よろしくお願いいたします

---
+++ Assets/BallController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+    private int sumscore=0;
+    private int degree = 0;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+    private GameObject FinalScoreText;
+  
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得 
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.FinalScoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+            this.FinalScoreText.GetComponent<Text>().text = "TotalScore :" + sumscore;
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        //ボールのスコア判定
+
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.sumscore += 1;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.sumscore += 5;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.sumscore += 2;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.sumscore += 10;
+        }
+
+       
+    }
+
+}
---
+++ Assets/StarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour {
+
+    //回転速度
+    private float rotSpeed = 1f;
+
+    //initialization
+    void Start(){
+        this.transform.Rotate(0, Random.Range(0, 360), 0);   
+    }
+
+    //Update is called once per frame
+    void Update(){
+
+        //回転
+        this.transform.Rotate(0, this.rotSpeed, 0);
+    }
+}
+
